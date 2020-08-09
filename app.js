2ddeab1203154d99acb6063d7beecc5f53d0f9bd
Anik Ghosh <tech.anikghosh@gmail.com>
2020-08-10 01:51:28 +0530

    start button and speech synthesis added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ window.onload = () => {
 
     // reference - https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition
 
-    var grammar = '#JSGF V1.0; grammar movements; public <key> = up | down | right | left | aap | ab | close | reload;'
+    var grammar = '#JSGF V1.0; grammar movements; public <key> = up | down | right | left | aap | ab | close | restart;'
     var recognition = new webkitSpeechRecognition();
     var speechRecognitionList = new webkitSpeechGrammarList();
     speechRecognitionList.addFromString(grammar, 1);
@@ -14,17 +14,23 @@ window.onload = () => {
     recognition.interimResults = false;
     recognition.maxAlternatives = 1;
 
+    var msg = new SpeechSynthesisUtterance();
+    msg.voiceURI = 'native';
+    msg.volume = 2;
+    msg.rate = 1.2;
+    msg.pitch = 1.5;
+    msg.lang = 'en';
     recognition.onend = function() {
         recognition.start();
     };
 
     let style = window.getComputedStyle(element);
 
-    // document.getElementById('start').onclick = function() {
-    //     console.log('Start Button clicked!!!');
-    //     recognition.start();
-    //     console.log('Ready to receive a color command.');
-    // };
+    document.getElementById('start').onclick = function() {
+        console.log('Start Button clicked!!!');
+        recognition.start();
+        console.log('Ready to receive a color command.');
+    };
 
     // document.getElementById('stop').onclick = function() {
     //     console.log('Stop Button clicked!!!');
@@ -32,7 +38,7 @@ window.onload = () => {
     //     console.log('Stopped receiving commands command.');
     // };
 
-    recognition.start();
+    // recognition.start();
 
 
     recognition.onresult = function(event) {
@@ -46,6 +52,8 @@ window.onload = () => {
             // up arrow
             console.log('up');
             command.value = 'up';
+            msg.text = 'going up';
+            window.speechSynthesis.speak(msg);
             bottom = style.getPropertyValue('bottom');
             if (parseInt(bottom.split('px')[0]) <= 275) element.style.bottom = parseInt(bottom.split('px')[0]) + 25 + 'px';
         }
@@ -53,6 +61,8 @@ window.onload = () => {
             // down arrow
             console.log('down');
             command.value = 'down';
+            msg.text = 'going down';
+            window.speechSynthesis.speak(msg);
             bottom = style.getPropertyValue('bottom');
             if (parseInt(bottom.split('px')[0]) >= 25)  element.style.bottom = parseInt(bottom.split('px')[0]) - 25 + 'px';
         }
@@ -60,6 +70,8 @@ window.onload = () => {
            // left arrow
            console.log('left');
            command.value = 'left';
+           msg.text = 'going left';
+           window.speechSynthesis.speak(msg);
            left = style.getPropertyValue('left');
            if (parseInt(left.split('px')[0]) >= 25) element.style.left = parseInt(left.split('px')[0]) - 25 + 'px';
         }
@@ -67,18 +79,27 @@ window.onload = () => {
            // right arrow
            console.log('right');
            command.value = 'right';
+           msg.text = 'going right';
+           window.speechSynthesis.speak(msg);
            left = style.getPropertyValue('left');
            if (parseInt(left.split('px')[0]) <= 275) element.style.left = parseInt(left.split('px')[0]) + 25 + 'px';
         }
         else if (key == 'close') {
             console.log('closing...');
             command.value = 'close';
-            window.close();
+            msg.text = 'closing';
+            window.speechSynthesis.speak(msg);
+            setTimeout(() => {
+                window.close();
+            }, 1000);
          }
-        else if (key == 'reload') {
-        console.log('reloading..');
-        command.value = 'reload';
-        location.reload();
+        else if (key == 'restart') {
+        console.log('restarting..');
+        command.value = 'restart';
+        msg.text = 'restarting';
+        window.speechSynthesis.speak(msg);
+        element.style.bottom = 0 + 'px';
+        element.style.left = 0 + 'px';
         }
     };
     
